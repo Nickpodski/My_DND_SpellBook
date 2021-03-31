037e532de9337d23ea1969d1bf69163b30b8f1bd
Nickpodski <Nickpodski@gmail.com>
2021-03-31 14:19:18 -0400

    finished being able to add or remove spells from a user

--- controllers/api/spellRoutes.js
+++ controllers/api/spellRoutes.js
@@ -9,7 +9,6 @@ router.post('/', async (req, res) => {
       ...req.body,
       // user_id: req.session.user_id,
     });
-
     res.status(200).json(newUserSpell);
   } catch (err) {
     res.status(400).json(err);
@@ -39,13 +38,18 @@ router.get('/:id', async (req, res) => {
         id: spellIDS,
       }
     })
-    res.status(200).json(findSpells);
+    const updatedUser = await User.findOne({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).json(updatedUser);
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.put('/:id', async (req, res) => {
+router.put('/:id/add', async (req, res) => {
   try {
     const addSpell = await User.findOne({
       where: {
@@ -54,58 +58,82 @@ router.put('/:id', async (req, res) => {
     });
     const spellID = req.body.spellbook;
     if (!addSpell) {
-      res.status(404).json({message: 'No user found with this id!'});
+      res.status(404).json({ message: 'No user found with this id!' });
       return;
     }
     const spellsSTR = addSpell.spellbook;
     if (spellsSTR != null) {
-      const spellIDS = spellsSTR.split(',').map(Number);
-      spellIDS.push(parseInt(spellID));
-      console.log(spellIDS);
-      const newspellIDS = spellIDS.join();
-      await User.update( {spellbook: newspellIDS},{
-        where: {
-          id: req.params.id,
-        },
-      });
+      if (spellsSTR.includes(parseInt(spellID))) {
+       res.status(409).json({ message: 'Spellbook already includes this spell for this user!'})
+       return;
+      } else {
+        const spellIDS = spellsSTR.split(',').map(Number);
+        spellIDS.push(parseInt(spellID));
+        const newspellIDS = spellIDS.join();
+        await User.update({ spellbook: newspellIDS }, {
+          where: {
+            id: req.params.id,
+          },
+        });
+      }
     } else {
-      await User.update( {spellbook: spellID},{
+      await User.update({ spellbook: spellID }, {
         where: {
           id: req.params.id,
         },
       });
     }
-    res.status(200).json(addSpell);
+    const updatedUser = await User.findOne({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).json(updatedUser);
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-// router.get('/:id/:spell', async (req, res) => {
-//   try {
-//     const getSpell = await User.findOne({
-//       where: {
-//         id: req.params.id,
-//         // user_id: req.session.user_id,
-//       },
-//     });
-//     if (!getSpell) {
-//       res.status(404).json({ message: 'No user found with this id!' });
-//       return;
-//     }
-//     const spellsSTR = getSpell.spellbook;
-//     const spellIDS = spellsSTR.split(',').map(Number);
-//     this.spell = params.get('spell');
-//     const spell = spellIDS[this.spell];
-//     const findSpell = await Spell.findOne({
-//       where: {
-//         id: spell,
-//       }
-//     })
-//     res.status(200).json(findSpell);
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
+router.put('/:id/remove', async (req, res) => {
+  try {
+    const deleteSpell = await User.findOne({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!deleteSpell) {
+      res.status(404).json({ message: 'No user found with this id!' });
+      return;
+    }
+    const spellsSTR = deleteSpell.spellbook;
+    if (spellsSTR == null) {
+      res.status(404).json({ message: 'No spellbook found with this user id!' })
+      return;
+    }
+    const spellID = req.body.spellbook;
+    const spellIDS = spellsSTR.split(',').map(Number);
+    const index = spellIDS.indexOf(parseInt(spellID));
+    if (index > -1) {
+      spellIDS.splice(index, 1);
+    } else {
+      res.status(404).json({ message: `This spell doesn't exist in this user's spellbook!` });
+      return;
+    }
+    const newSpellIDS = spellIDS.join();
+    await User.update({ spellbook: newSpellIDS }, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    const updatedUser = await User.findOne({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).json(updatedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
