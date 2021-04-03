1237de0f72c3799e45ef668855b2c2439d9a7586
Nickpodski <Nickpodski@gmail.com>
2021-04-02 21:24:18 -0400

    changed connection file

--- config/connection.js
+++ config/connection.js
@@ -10,6 +10,7 @@ if (process.env.JAWSDB_URL) {
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
+    process.env.VUE_APP_API_ENDPOINT,
     {
       host: 'localhost',
       dialect: 'mysql',
